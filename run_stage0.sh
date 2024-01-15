34d50e674dc44bdf3ea48cf311d529d0e0d5e702
Freedomcls <1978500828@qq.com>
2024-01-15 16:45:27 +0800

    first commit

---
+++ run_stage0.sh
@@ -0,0 +1,4 @@
+python train.py --dataset_name replica --root_dir ./room_0/Sequence_1/ \
+    --N_importance 64 --img_wh 320 240 --num_epochs 18 --batch_size 4096 --optimizer adam \
+    --lr 0.0005 --lr_scheduler steplr --decay_step 4 8 --decay_gamma 0.5 --exp_name replica_stage0 \
+    --loss_type mse --chunk 40000 --nerf_model NeRF --num_gpus 4
